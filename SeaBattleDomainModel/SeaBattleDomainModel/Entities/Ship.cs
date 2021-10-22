fd31a680e2e95beb9d226cf54843d38025970828
Denys Boryhin <denys.boryhin@nixsolutions.com>
2021-10-22 14:28:20 +0300

    Code of ship class was changed

--- SeaBattleDomainModel/SeaBattleDomainModel/Entities/Ship.cs
+++ SeaBattleDomainModel/SeaBattleDomainModel/Entities/Ship.cs
@@ -6,7 +6,58 @@ using System.Threading.Tasks;
 
 namespace SeaBattleDomainModel.Entities
 {
-    class Ship
+    public abstract class Ship : IEquatable<Ship>
     {
+        public abstract int Velocity { get; set; }
+
+        public abstract int Range { get; set; }
+
+        public abstract int Size { get; set; }
+
+        public static bool operator ==(Ship ship1, Ship ship2)
+        {
+            //TODO: реализовать перегрузку оператора сравнения
+            return ship1.Velocity == ship2.Velocity;
+        }
+
+        public static bool operator !=(Ship ship1, Ship ship2)
+        {
+            //TODO: реализовать перегрузку оператора сравнения
+            return ship1.Velocity != ship2.Velocity;
+        }
+
+        //public void FillShipPoints(Point head, Point tail)
+        //{
+        //    if (head.Equals(tail))
+        //    {
+        //        Points = new Point[1] { head }; //имеем корабль-точку
+        //    }
+        //}
+
+        public abstract void Move();
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as Ship);
+        }
+
+        public bool Equals(Ship other)
+        {
+            return other != null &&
+                   Velocity == other.Velocity &&
+                   Range == other.Range &&
+                   Size == other.Size;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Velocity, Range);
+        }
+
+        public override string ToString()
+        {
+            //TODO: реализовать переопределние для вывода состояния в строку
+            return base.ToString();
+        }
     }
 }
