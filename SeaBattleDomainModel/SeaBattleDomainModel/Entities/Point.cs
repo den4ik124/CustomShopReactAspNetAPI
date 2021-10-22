ba350f53c8ea04d44bd23e29c0ce08971bd3a9a5
Denys Boryhin <denys.boryhin@nixsolutions.com>
2021-10-22 14:29:37 +0300

    The code of struct Point was changed

--- SeaBattleDomainModel/SeaBattleDomainModel/Entities/Point.cs
+++ SeaBattleDomainModel/SeaBattleDomainModel/Entities/Point.cs
@@ -6,7 +6,98 @@ using System.Threading.Tasks;
 
 namespace SeaBattleDomainModel.Entities
 {
-    internal struct Point
+    public struct Point : IEquatable<Point>
     {
+        private int x;
+
+        private int y;
+
+        private Quadrant quadrantId;
+
+        private int xQuad;
+
+        private int yQuad;
+
+        /// <summary>
+        /// Переопределение конструктора по умолчанию
+        /// </summary>
+        //public Point(int x, int y)
+        //{
+        //    //TODO: проиниализировать все поля
+        //}
+
+        public int X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+
+        public int Y
+        {
+            get { return y; }
+            set { y = value; }
+        }
+
+        public int XQuad { get; set; }
+        public int YQuad { get; set; }
+
+        public Quadrant Quadrant
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Point point && Equals(point);
+        }
+
+        public bool Equals(Point other)
+        {
+            return x == other.x &&
+                   y == other.y &&
+                   quadrantId == other.quadrantId &&
+                   xQuad == other.xQuad &&
+                   yQuad == other.yQuad &&
+                   X == other.X &&
+                   Y == other.Y &&
+                   XQuad == other.XQuad &&
+                   YQuad == other.YQuad &&
+                   Quadrant == other.Quadrant;
+        }
+
+        public override int GetHashCode()
+        {
+            HashCode hash = new HashCode();
+            hash.Add(x);
+            hash.Add(y);
+            hash.Add(quadrantId);
+            hash.Add(xQuad);
+            hash.Add(yQuad);
+            hash.Add(X);
+            hash.Add(Y);
+            hash.Add(XQuad);
+            hash.Add(YQuad);
+            hash.Add(Quadrant);
+            return hash.ToHashCode();
+        }
+
+        /// <summary>
+        /// Получает координаты точки. Определяет квадрант
+        /// </summary>
+        private Quadrant GetQuadrant(int x, int y)
+        {
+            return default;
+        }
+
+        /// <summary>
+        /// По квадранту определяет координаты точек относительно квадранта
+        /// </summary>
+        private void RecalculateQuadrantCoordinates()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
