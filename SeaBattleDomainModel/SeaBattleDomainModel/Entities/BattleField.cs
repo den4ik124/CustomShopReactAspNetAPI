4e3c9ac39b84d865e997c8d8e0bf5c4e0ae3947d
Denys Boryhin <denys.boryhin@nixsolutions.com>
2021-10-22 14:26:50 +0300

    BattleField ship code was changed

--- SeaBattleDomainModel/SeaBattleDomainModel/Entities/BattleField.cs
+++ SeaBattleDomainModel/SeaBattleDomainModel/Entities/BattleField.cs
@@ -6,7 +6,114 @@ using System.Threading.Tasks;
 
 namespace SeaBattleDomainModel.Entities
 {
-    class BattleField
+    internal class BattleField
     {
+        private List<Ship> ships;
+
+        private int size;
+
+        private Cell[] cells;
+
+        public BattleField(int size)
+        {
+            this.size = size;
+            this.ships = new List<Ship>();
+            this.cells = FillCells(size);
+        }
+
+        public Ship this[Quadrant quadrant, int x, int y]
+        {
+            get
+            {
+                return GetShipByCoordinates(quadrant, x, y);
+            }
+        }
+
+        private Cell[] FillCells(int size)
+        {
+            return new Cell[1];
+        }
+
+        /// <summary>
+        /// Находит корабль в списке кораблей
+        /// </summary>
+        /// <returns>Корабль по переданным параметрам</returns>
+        private Ship GetShipByCoordinates(Quadrant quadrant, int x, int y)
+        {
+            //TODO: реализовать логику возврата корабля
+            return null;
+        }
+
+        public void AddShip(Ship ship, Point head, Point tail)
+        {
+            if (IsLocationValid(head, tail) == false)
+            {
+                return;
+            }
+            //TODO: реализовать инициализацию и заполнение массива точек ссылками на корабль
+            this.ships.Add(ship);
+        }
+
+        /// <summary>
+        /// Определяет можно ли расположить корабль по указанным координатам
+        /// </summary>
+        /// <returns>
+        /// true - корабль можно расположить по указанным координатам
+        /// false - корабль нельзя расположить по указанным координатам
+        /// </returns>
+        private bool IsLocationValid(Point head, Point tail)
+        {
+            return CheckOutOfBoundaries(head, tail) && CheckOrientation(head, tail) && CheckCollision(head, tail) && CheckNeighborhoods(head, tail);
+        }
+
+        /// <summary>
+        /// Определяет вертикальный или горизонтальный отрезок
+        /// </summary>
+        /// <param name="head">Точка "носа" корабля</param>
+        /// <param name="tail">Точка кормы корабля</param>
+        /// <returns>true - если линия вертикальная/горизонтальная. false - если линия под углом.</returns>
+        private bool CheckOrientation(Point head, Point tail)
+        {
+            if (head.X != tail.X && head.Y == tail.Y //горизонтальная линия
+                || head.Y != tail.Y && head.X == tail.Y)
+                return true;
+            return false;
+        }
+
+        /// <summary>
+        /// Метод проверяет нет ли между указанными точками других кораблей (других точек)
+        /// </summary>
+        /// <param name="head">Координаты "носа" корабля</param>
+        /// <param name="tail">Координаты кормы корабля</param>
+        /// <returns>true - если пересечения нет, false - если пересечение есть</returns>
+        private bool CheckCollision(Point head, Point tail)
+        {
+            //TODO: реализовать проверку пересечения
+            return true;
+        }
+
+        /// <summary>
+        /// Проверка не находится ли хотя бы одна из точек за пределами карты
+        /// </summary>
+        /// <param name="head">Координаты "носа" корабля</param>
+        /// <param name="tail">Координаты кормы корабля</param>
+        /// <returns>true - если все точки в пределах карты, false - хотя бы одна точка за пределами карты</returns>
+        private bool CheckOutOfBoundaries(Point head, Point tail)
+        {
+            //TODO: реализовать проверку выхода за карту
+            return true;
+        }
+
+        private bool CheckNeighborhoods(Point head, Point tail)
+        {
+            //TODO: реализовать проверку на расположение кораблей с интервалом
+            return true;
+        }
+
+        public override string ToString()
+        {
+            //TODO: реализовать переопределение
+            return base.ToString();
+        }
     }
 }
