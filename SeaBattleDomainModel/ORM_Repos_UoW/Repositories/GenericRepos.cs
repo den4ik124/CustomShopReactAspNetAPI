16b0a2e0fa4a0d10c52d7fb40d07a24373186b0b
Denys Boryhin <denys.boryhin@nixsolutions.com>
2021-11-25 19:30:28 +0200

    Light refactoring was implemented

--- SeaBattleDomainModel/ORM_Repos_UoW/Repositories/GenericRepos.cs
+++ SeaBattleDomainModel/ORM_Repos_UoW/Repositories/GenericRepos.cs
@@ -34,166 +34,6 @@ namespace ORM_Repos_UoW.Repositories
 
         #region Methods
 
-        #region Methods.Private
-
-        //private void GetSinglePropertyData(Type type, out string tableName, out List<string> propertiesNames)
-        //{
-        //    var attribute = type.GetCustomAttribute<TableAttribute>();
-        //    tableName = attribute.TableName;
-        //    if (attribute.IsRelatedTable)
-        //    {
-        //        tableName += $".rel";
-        //    }
-        //    propertiesNames = type.GetProperties()
-        //                            .Where(prop => prop.GetCustomAttributes<ColumnAttribute>().Count() > 0)
-        //                            .Select(atr => atr.GetCustomAttribute<ColumnAttribute>().ColumnName).ToList();
-        //    if (type.IsAbstract)
-        //    {
-        //        var columnMatching = assembly.GetTypes()
-        //                                    .Where(t => t.GetCustomAttributes<InheritanceRelationAttribute>().Count() > 0
-        //                                    && t.GetCustomAttribute<InheritanceRelationAttribute>().IsBaseClass == false)
-        //                                    .Select(a => a.GetCustomAttribute<InheritanceRelationAttribute>().ColumnMatching)
-        //                                    .First();
-        //        //string propertyName = $"{columnMatching} AS  {tableName}{columnMatching}";
-        //        string propertyName = $"{columnMatching}";
-        //        propertiesNames.Add(propertyName);
-        //    }
-        //}
-
-        private object MatchDataItem(Type type, SqlDataReader reader)
-        {
-            object item;
-            if (type.IsAbstract)
-            {
-                item = GetDerivedClass(reader);
-                if (item == null)
-                {
-                    return item;
-                }
-            }
-            else
-                item = Activator.CreateInstance(type);
-            var columns = type.GetProperties()
-                                .Where(prop => prop.GetCustomAttributes<ColumnAttribute>().Count() > 0)
-                                .Select(atr => atr.GetCustomAttribute<ColumnAttribute>().ColumnName);
-            var properties = type.GetProperties().Where(prop => prop.GetCustomAttributes<ColumnAttribute>().Count() > 0);
-            FillProperties(reader, ref item, properties);
-
-            var childs = type.GetProperties().Where(prop => prop.GetCustomAttributes<RelatedEntityAttribute>().Count() > 0);
-
-            if (childs.Count() == 0)
-            {
-                return item;
-            }
-
-            FillChilds(reader, ref item, childs);
-
-            return item;
-        }
-
-        private void FillChilds(SqlDataReader reader, ref object item, IEnumerable<PropertyInfo> childs)
-        {
-            foreach (var child in childs)
-            {
-                var childType = child.GetCustomAttribute<RelatedEntityAttribute>().RelatedType;
-                var isCollection = child.GetCustomAttribute<RelatedEntityAttribute>().IsCollection;
-                if (isCollection)
-                {
-                    //var test = FilledCollection(child, reader);
-                    //var listType = child.PropertyType;
-                    //var countTest = listType.GetProperty("Count").GetValue(test);
-                    var itemId = (int)child.ReflectedType.GetProperties().First(p => p.GetCustomAttribute<ColumnAttribute>().KeyType == KeyType.Primary).GetValue(item);
-                    child.SetValue(item, GetFilledCollection(child, itemId, reader));
-                }
-                else
-                {
-                    child.SetValue(item, MatchDataItem(childType, reader));
-                }
-            }
-        }
-
-        private object? GetFilledCollection(PropertyInfo child, int id, SqlDataReader reader)
-        {
-            var baseType = child.ReflectedType;
-            var baseTypePrimaryColumnName = $"{baseType.GetCustomAttribute<TableAttribute>().TableName}" +
-                                            $"{baseType.GetProperties().First(p => p.GetCustomAttribute<ColumnAttribute>().KeyType == KeyType.Primary).Name}";
-
-            //var baseTypeId = baseType.GetProperties().First(prop => prop.GetCustomAttribute<ColumnAttribute>().KeyType == KeyType.Primary);
-            var baseTypeId = id;
-            var type = child.PropertyType;
-            var collection = Activator.CreateInstance(type);
-            var genericTypes = child.PropertyType.GenericTypeArguments;
-            var itemsCollection = new List<object>();
-
-            object item;
-            if (reader.HasRows)
-            {
-                do
-                {
-                    if ((int)reader[baseTypePrimaryColumnName] != baseTypeId)
-                    {
-                        break;
-                    }
-                    foreach (var generic in genericTypes)
-                    {
-                        item = MatchDataItem(generic, reader);
-                        if (item == null)
-                        {
-                            continue;
-                        }
-                        itemsCollection.Add(item);
-                    }
-                    if (itemsCollection.Count != 0)
-                    {
-                        var methodAdd = child.PropertyType.GetMethod("Add");
-                        methodAdd.Invoke(collection, itemsCollection.ToArray());
-                        itemsCollection.Clear();
-                    }
-                } while (reader.Read()); //TODO: сжирает строку следующего BF. Подумать как исправить (Только при ReadItems)
-            }
-            return collection;
-        }
-
-        private object GetDerivedClass(SqlDataReader reader)
-        {
-            Type type = typeof(object);
-            var derivedTypes = assembly.GetTypes().Where(t => t.GetCustomAttributes<InheritanceRelationAttribute>().Count() > 0
-                                                        && t.GetCustomAttribute<InheritanceRelationAttribute>().IsBaseClass == false);
-            var tableName = derivedTypes.First().GetCustomAttribute<TableAttribute>().TableName;
-            var matchingColumnName = derivedTypes.FirstOrDefault().GetCustomAttribute<TypeAttribute>().ColumnMatching;
-            if (matchingColumnName.EndsWith("id", StringComparison.OrdinalIgnoreCase))
-            {
-                matchingColumnName = $"{tableName}{matchingColumnName}";
-            }
-
-            if (reader[matchingColumnName].GetType() == typeof(DBNull))
-            {
-                return null;
-            }
-
-            type = derivedTypes.FirstOrDefault(t => t.GetCustomAttribute<TypeAttribute>().TypeID == (int)reader[matchingColumnName]);
-            return Activator.CreateInstance(type);
-        }
-
-        private void FillProperties(SqlDataReader reader, ref object? item, IEnumerable<PropertyInfo> properties)
-        {
-            foreach (var prop in properties)
-            {
-                string columnName = prop.GetCustomAttribute<ColumnAttribute>().ColumnName;
-                if (columnName.EndsWith("id", StringComparison.OrdinalIgnoreCase))
-                {
-                    columnName = $"{prop.ReflectedType.GetCustomAttribute<TableAttribute>().TableName}Id";
-                }
-
-                if (reader[columnName].GetType() != typeof(DBNull))
-                    prop.SetValue(item, reader[columnName]);
-                else
-                    prop.SetValue(item, null);
-            }
-        }
-
-        #endregion Methods.Private
-
         #region Methods.Public
 
         public void Create(T item)
@@ -321,6 +161,233 @@ namespace ORM_Repos_UoW.Repositories
         }
 
         #endregion Methods.Public
+
+        #region Methods.Private
+
+        //private void GetSinglePropertyData(Type type, out string tableName, out List<string> propertiesNames)
+        //{
+        //    var attribute = type.GetCustomAttribute<TableAttribute>();
+        //    tableName = attribute.TableName;
+        //    if (attribute.IsRelatedTable)
+        //    {
+        //        tableName += $".rel";
+        //    }
+        //    propertiesNames = type.GetProperties()
+        //                            .Where(prop => prop.GetCustomAttributes<ColumnAttribute>().Count() > 0)
+        //                            .Select(atr => atr.GetCustomAttribute<ColumnAttribute>().ColumnName).ToList();
+        //    if (type.IsAbstract)
+        //    {
+        //        var columnMatching = assembly.GetTypes()
+        //                                    .Where(t => t.GetCustomAttributes<InheritanceRelationAttribute>().Count() > 0
+        //                                    && t.GetCustomAttribute<InheritanceRelationAttribute>().IsBaseClass == false)
+        //                                    .Select(a => a.GetCustomAttribute<InheritanceRelationAttribute>().ColumnMatching)
+        //                                    .First();
+        //        //string propertyName = $"{columnMatching} AS  {tableName}{columnMatching}";
+        //        string propertyName = $"{columnMatching}";
+        //        propertiesNames.Add(propertyName);
+        //    }
+        //}
+
+        private object MatchDataItem(Type type, SqlDataReader reader)
+        {
+            var item = GetItemInstance(type, reader);
+            if (item == null)
+            {
+                return null;
+            }
+            item = FillProperties(item, type, reader);
+            item = FillChilds(item, type, reader);
+
+            return item;
+        }
+
+        private object GetItemInstance(Type type, SqlDataReader reader)
+        {
+            object item;
+            if (type.IsAbstract)
+            {
+                item = GetDerivedClass(reader);
+                if (item == null)
+                {
+                    return item;
+                }
+            }
+            else
+                item = Activator.CreateInstance(type);
+            return item;
+        }
+
+        //private void FillChilds(SqlDataReader reader, ref object item, IEnumerable<PropertyInfo> childs)
+        //{
+        //    foreach (var child in childs)
+        //    {
+        //        var childType = child.GetCustomAttribute<RelatedEntityAttribute>().RelatedType;
+        //        var isCollection = child.GetCustomAttribute<RelatedEntityAttribute>().IsCollection;
+        //        if (isCollection)
+        //        {
+        //            //var test = FilledCollection(child, reader);
+        //            //var listType = child.PropertyType;
+        //            //var countTest = listType.GetProperty("Count").GetValue(test);
+        //            var itemId = (int)child.ReflectedType.GetProperties().First(p => p.GetCustomAttribute<ColumnAttribute>().KeyType == KeyType.Primary).GetValue(item);
+        //            child.SetValue(item, GetFilledCollection(child, itemId, reader));
+        //        }
+        //        else
+        //        {
+        //            child.SetValue(item, MatchDataItem(childType, reader));
+        //        }
+        //    }
+        //}
+
+        //private object FillChilds(SqlDataReader reader, object item, IEnumerable<PropertyInfo> childs)
+        //{
+        //    foreach (var child in childs)
+        //    {
+        //        var childType = child.GetCustomAttribute<RelatedEntityAttribute>().RelatedType;
+        //        var isCollection = child.GetCustomAttribute<RelatedEntityAttribute>().IsCollection;
+        //        if (isCollection)
+        //        {
+        //            //var test = FilledCollection(child, reader);
+        //            //var listType = child.PropertyType;
+        //            //var countTest = listType.GetProperty("Count").GetValue(test);
+        //            var itemId = (int)child.ReflectedType.GetProperties().First(p => p.GetCustomAttribute<ColumnAttribute>().KeyType == KeyType.Primary).GetValue(item);
+        //            child.SetValue(item, GetFilledCollection(child, itemId, reader));
+        //        }
+        //        else
+        //        {
+        //            child.SetValue(item, MatchDataItem(childType, reader));
+        //        }
+        //    }
+        //    return item;
+        //}
+        private object FillChilds(object item, Type type, SqlDataReader reader)
+        {
+            var childs = type.GetProperties().Where(prop => prop.GetCustomAttributes<RelatedEntityAttribute>().Count() > 0);
+
+            if (childs.Count() == 0)
+            {
+                return item;
+            }
+
+            foreach (var child in childs)
+            {
+                var childType = child.GetCustomAttribute<RelatedEntityAttribute>().RelatedType;
+                var isCollection = child.GetCustomAttribute<RelatedEntityAttribute>().IsCollection;
+                if (isCollection)
+                {
+                    //var test = FilledCollection(child, reader);
+                    //var listType = child.PropertyType;
+                    //var countTest = listType.GetProperty("Count").GetValue(test);
+                    var itemId = (int)child.ReflectedType.GetProperties().First(p => p.GetCustomAttribute<ColumnAttribute>().KeyType == KeyType.Primary).GetValue(item);
+                    child.SetValue(item, GetFilledCollection(child, itemId, reader));
+                }
+                else
+                {
+                    child.SetValue(item, MatchDataItem(childType, reader));
+                }
+            }
+            return item;
+        }
+
+        private object? GetFilledCollection(PropertyInfo child, int id, SqlDataReader reader)
+        {
+            var baseType = child.ReflectedType;
+            var baseTypePrimaryColumnName = $"{baseType.GetCustomAttribute<TableAttribute>().TableName}" +
+                                            $"{baseType.GetProperties().First(p => p.GetCustomAttribute<ColumnAttribute>().KeyType == KeyType.Primary).Name}";
+
+            //var baseTypeId = baseType.GetProperties().First(prop => prop.GetCustomAttribute<ColumnAttribute>().KeyType == KeyType.Primary);
+            var baseTypeID = id;
+            var type = child.PropertyType;
+            var collection = Activator.CreateInstance(type);
+            var genericTypes = child.PropertyType.GenericTypeArguments;
+            var itemsCollection = new List<object>();
+
+            object item;
+            if (reader.HasRows)
+            {
+                do
+                {
+                    if ((int)reader[baseTypePrimaryColumnName] != baseTypeID)
+                    {
+                        break;
+                    }
+                    foreach (var generic in genericTypes)
+                    {
+                        item = MatchDataItem(generic, reader);
+                        if (item == null)
+                        {
+                            continue;
+                        }
+                        itemsCollection.Add(item);
+                    }
+                    if (itemsCollection.Count != 0)
+                    {
+                        var methodAdd = child.PropertyType.GetMethod("Add");
+                        methodAdd.Invoke(collection, itemsCollection.ToArray());
+                        itemsCollection.Clear();
+                    }
+                } while (reader.Read()); //TODO: сжирает строку следующего BF. Подумать как исправить (Только при ReadItems)
+            }
+            return collection;
+        }
+
+        private object GetDerivedClass(SqlDataReader reader)
+        {
+            Type type = typeof(object);
+            var derivedTypes = assembly.GetTypes().Where(t => t.GetCustomAttributes<InheritanceRelationAttribute>().Count() > 0
+                                                        && t.GetCustomAttribute<InheritanceRelationAttribute>().IsBaseClass == false);
+            var tableName = derivedTypes.First().GetCustomAttribute<TableAttribute>().TableName;
+            var matchingColumnName = derivedTypes.FirstOrDefault().GetCustomAttribute<TypeAttribute>().ColumnMatching;
+            if (matchingColumnName.EndsWith("id", StringComparison.OrdinalIgnoreCase))
+            {
+                matchingColumnName = $"{tableName}{matchingColumnName}";
+            }
+
+            if (reader[matchingColumnName].GetType() == typeof(DBNull))
+            {
+                return null;
+            }
+
+            type = derivedTypes.FirstOrDefault(t => t.GetCustomAttribute<TypeAttribute>().TypeID == (int)reader[matchingColumnName]);
+            return Activator.CreateInstance(type);
+        }
+
+        //private void FillProperties(SqlDataReader reader, ref object? item, IEnumerable<PropertyInfo> properties)
+        //{
+        //    foreach (var prop in properties)
+        //    {
+        //        string columnName = prop.GetCustomAttribute<ColumnAttribute>().ColumnName;
+        //        if (columnName.EndsWith("id", StringComparison.OrdinalIgnoreCase))
+        //        {
+        //            columnName = $"{prop.ReflectedType.GetCustomAttribute<TableAttribute>().TableName}Id";
+        //        }
+
+        //        if (reader[columnName].GetType() != typeof(DBNull))
+        //            prop.SetValue(item, reader[columnName]);
+        //        else
+        //            prop.SetValue(item, null);
+        //    }
+        //}
+        private object FillProperties(object item, Type type, SqlDataReader reader)
+        {
+            var properties = type.GetProperties().Where(prop => prop.GetCustomAttributes<ColumnAttribute>().Count() > 0);
+
+            foreach (var prop in properties)
+            {
+                string columnName = prop.GetCustomAttribute<ColumnAttribute>().ColumnName;
+                if (columnName.EndsWith("id", StringComparison.OrdinalIgnoreCase))
+                {
+                    columnName = $"{prop.ReflectedType.GetCustomAttribute<TableAttribute>().TableName}Id";
+                }
+
+                if (reader[columnName].GetType() != typeof(DBNull))
+                    prop.SetValue(item, reader[columnName]);
+                else
+                    prop.SetValue(item, null);
+            }
+            return item;
+        }
+
+        #endregion Methods.Private
 
         #endregion Methods
     }
